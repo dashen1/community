60d1059d08d68acc37d463fe065b4d8824a603e2
yuanma <yuanma123456@qq.com>
2020-02-19 22:09:58 +0800

    config file properties

---
+++ src/main/java/life/yuanma/community/controller/AuthorizeController.java
@@ -0,0 +1,44 @@
+package life.yuanma.community.controller;
+
+import life.yuanma.community.controller.provider.GithubProvider;
+import life.yuanma.community.dto.AccessTokenDTO;
+import life.yuanma.community.dto.GithubUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) throws IOException {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        String name = githubUser.getName();
+        System.out.println(name);
+        return "index";
+    }
+}
