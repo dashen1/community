d0133c32459bba9a8c2773e83227dc5af4b84bcc
yuanma <yuanma123456@qq.com>
2020-02-20 17:09:08 +0800

    cookies

--- src/main/java/life/yuanma/community/controller/IndexController.java
+++ src/main/java/life/yuanma/community/controller/IndexController.java
@@ -1,12 +1,34 @@
 package life.yuanma.community.controller;
 
+import life.yuanma.community.mapper.UserMapper;
+import life.yuanma.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request)
+    {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie:cookies) {
+            if (cookie.getName().equals(("token"))){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
