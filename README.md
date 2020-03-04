05587171273ad0ec09fc06dd1657027681a1c0f8
yuanma <yuanma123456@qq.com>
2020-03-04 09:22:59 +0800

    1

--- README.md
+++ README.md
@@ -29,3 +29,9 @@ mvn -Dmybatis.generator.overwrite=true mybatis-generator:generate
              <version>1.4.0</version>
          </dependency>
 ##
+##
+解决办法：不用上述方式编译打包，改用命令行方式跳过Test，命令如下：
+mvn clean package -DskipTests
+java -jar -Dspring.profiles.active=production target/community-0.0.1-SNAPSHOT.jar
+这样maven就不会进行Test相关的编译了。
+##
--- src/main/java/life/yuanma/community/controller/IndexController.java
+++ src/main/java/life/yuanma/community/controller/IndexController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
@@ -17,9 +18,9 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model,
-                        @RequestParam(name = "page",defaultValue = "1") Integer page,
-                        @RequestParam(name = "size",defaultValue = "2") Integer size,
-                        @RequestParam(name = "search",required = false) String search) {
+                              @RequestParam(name = "page",defaultValue = "1") Integer page,
+                              @RequestParam(name = "size",defaultValue = "2") Integer size,
+                              @RequestParam(name = "search",required = false) String search) {
         PaginationDTO pagination = questionService.getList(page,size,search);
         model.addAttribute("pagination",pagination);
         model.addAttribute("search",search);
