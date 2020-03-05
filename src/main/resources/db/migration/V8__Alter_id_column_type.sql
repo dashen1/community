56ea65b01f9278c2b3e07db87eee7862da3c1325
yuanma <yuanma123456@qq.com>
2020-03-05 14:40:14 +0800

    m5

--- src/main/resources/db/migration/V8__Alter_id_column_type.sql
+++ src/main/resources/db/migration/V8__Alter_id_column_type.sql
@@ -1,4 +1,4 @@
 -- alter table QUESTION alter column ID bigint auto_increment;
 -- alter table "USER" alter column ID bigint auto_increment;
 alter table QUESTION modify ID bigint auto_increment NOT NULL;
-alter table "USER" modify ID bigint auto_increment NOT NULL;
+alter table USER modify ID bigint auto_increment NOT NULL;
