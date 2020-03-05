bf38369cf95e1451ae7d0d6d31fd0694719ac730
yuanma <yuanma123456@qq.com>
2020-03-05 14:35:38 +0800

    m5

--- src/main/resources/db/migration/V13__Alter_notification_table.sql
+++ src/main/resources/db/migration/V13__Alter_notification_table.sql
@@ -1,2 +1,2 @@
 -- alter table NOTIFICATION alter column ID BIGINT auto_increment;
-alter table NOTIFICATION modify ID BIGINT auto_increment;
+alter table NOTIFICATION modify ID BIGINT auto_increment NOT NULL;
