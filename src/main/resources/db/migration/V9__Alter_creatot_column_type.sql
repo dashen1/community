5acaa4f42e0a74d15a0f4ed3eb7f7fad3f47d757
yuanma <yuanma123456@qq.com>
2020-03-05 14:32:32 +0800

    m5

--- src/main/resources/db/migration/V9__Alter_creatot_column_type.sql
+++ src/main/resources/db/migration/V9__Alter_creatot_column_type.sql
@@ -1,4 +1,4 @@
 -- alter table QUESTION alter column CREATOR bigint auto_increment;
 -- alter table COMMENT alter column COMMENTATOR bigint auto_increment;
-alter table QUESTION modified CREATOR bigint auto_increment;
-alter table COMMENT modified COMMENTATOR bigint auto_increment;
+alter table QUESTION modify CREATOR bigint auto_increment;
+alter table COMMENT modify COMMENTATOR bigint auto_increment;
