e98afe4b959c19e80bc5ca8e6928bdf5fbeb176e
yuanma <yuanma123456@qq.com>
2020-04-21 09:41:13 +0800

    重新选购数据库

--- src/test/java/life/yuanma/community/CommunityApplicationTests.java
+++ src/test/java/life/yuanma/community/CommunityApplicationTests.java
@@ -1,10 +1,13 @@
 package life.yuanma.community;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CommunityApplicationTests {
+
+    @Autowired
 
     @Test
     void contextLoads() {
